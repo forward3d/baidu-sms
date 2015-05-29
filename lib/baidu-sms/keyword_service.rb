4d98caf7aa8b9cd5afa794fc9869b065352a1658
Andy Sykes <github@tinycat.co.uk>
2015-05-29 01:09:32 +0100

    Baidu API can return empty responses

--- lib/baidu-sms/keyword_service.rb
+++ lib/baidu-sms/keyword_service.rb
@@ -12,6 +12,8 @@ module BaiduSMS
     # {:keyword_id=>"1", :adgroup_id=>"1", :keyword=>"something", :price=>"1.00", :pc_destination_url=>"url", :match_type=>"2", :pause=>false, :status=>"41"}
     def get_keyword_by_adgroup_id(*adgroup_ids)
       response = @client.call(:get_keyword_by_adgroup_id, message: { adgroupIds: adgroup_ids })
+      # If there are no keywords in the adgroup, we'll be missing the subelements
+      return [] unless response.body[:get_keyword_by_adgroup_id_response][:group_keywords]
       keyword_info = response.body[:get_keyword_by_adgroup_id_response][:group_keywords][:keyword_types]
       # When we get a single keyword, it's a hash, not an array; coerce it into an array
       keyword_info = keyword_info.is_a?(Hash) ? [keyword_info] : keyword_info
